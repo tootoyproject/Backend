351ce36d09cc766f0a19a998ffc65b94ef053ab4
DongGuk-Seo <failnxid@gmail.com>
2022-11-18 16:21:35 +0900

    add swagger

--- api/settings.py
+++ api/settings.py
@@ -64,6 +64,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'dj_rest_auth.registration',
+    'drf_yasg'
 ]
 
 MIDDLEWARE = [
--- api/urls.py
+++ api/urls.py
@@ -14,15 +14,34 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from rest_framework import routers
+from django.urls import path, include, re_path
 from posts.views import PostsViewSet
+from rest_framework import routers
+from rest_framework.permissions import AllowAny
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 router = routers.DefaultRouter()
 router.register(r'lists', PostsViewSet)
 
+schema_url_patterns = [ 
+    path('', include('posts.urls')),
+    path('', include('users.urls')),
+    ]
+
+schema_view_v1 = get_schema_view(
+    openapi.Info(
+        title="Open API",
+        default_version='v1',
+        description="시스템 API",
+        terms_of_service="https://www.google.com/policies/terms/",
+    ),
+    public=True,
+    permission_classes=(AllowAny,),
+    patterns=schema_url_patterns,
+)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
@@ -31,4 +50,7 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view_v1.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view_v1.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view_v1.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
